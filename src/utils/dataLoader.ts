b6fce3fb82b1391d3a70f66d04d9152f95cee934
ardhiansyahputra <ardhimetanaputra@gmail.com>
2019-10-15 22:47:14 +0700

    update version graphql

--- src/utils/dataLoader.ts
+++
@@ -1,5 +0,0 @@
-import DataLoader from 'dataloader';
-
-export default (fn: DataLoader.BatchLoadFn<any, any>, options?: DataLoader.Options<any, any>) => {
-  return new DataLoader(fn, { cache: true, ...options });
-};
